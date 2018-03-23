076327da7b44e7dd0d86e33bdda0e695895fb607
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-23 19:50:29 +0100

    Update cogtest.py

--- cogtest/cogtest.py
+++ cogtest/cogtest.py
@@ -20,12 +20,12 @@ class cogtest:
     @commands.command(pass_context = True, no_pm = True)
     async def pfp(self, ctx, user: discord.Member = None):
         """Sends the user profile picture, if user is none then sends yours"""
-        m = 'Profile Picture for {}\n{}'
+        m = ('Profile Picture for **{}**:\n{}'.format(user.name, user.avatar_url))
         if user is None:
             user = ctx.message.author
-            await self.bot.say(m.format(user.name, user.avatar_url))
+            await self.bot.say(m)
         else:
-            await self.bot.say(m.format(user.name, user.avatar_url))
+            await self.bot.say(m)
             
 def setup(bot):
     n = cogtest(bot)
