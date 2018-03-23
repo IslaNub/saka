e5ea23eba3ff0578fe39cbfcc384231163577388
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-23 12:32:01 +0100

    Update cogtest.py

--- cogtest/cogtest.py
+++ cogtest/cogtest.py
@@ -6,14 +6,15 @@ from __main__ import send_cmd_help
 
 class cogtest:
     """cogtest"""
+    
     def __init__(self, bot):
         self.bot = bot
     
-    async def bhavya(self, *,  message):
+    async def bhavya(self, message):
         """Sends a message to Bhavya"""
         b = self.bot.get_user('383190461512155136')
         await self.bot.send_message(b, message)
-        await self.bot.say('Sent message to {}'.format(u.mention)
+        await self.bot.say('Sent message to {}'.format(u.mention))
 
 def setup(bot):
     n = cogtest(bot)
