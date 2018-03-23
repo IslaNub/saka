b491256daa5aedf589c01308f741a7d4983931f4
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-23 12:19:11 +0100

    Create cogtest.py

---
+++ cogtest/cogtest.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+from .utils.dataIO import fileIO
+from .utils import checks
+from __main__ import send_cmd_help
+
+class cogtest:
+    def __init__(self, bot):
+        self.bot = bot
+    
