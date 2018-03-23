540234c8612cfa45dcfaf2a780baf9dfd6577310
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-23 12:57:31 +0100

    Update cogtest.py

--- cogtest/cogtest.py
+++ cogtest/cogtest.py
@@ -13,12 +13,13 @@ class cogtest:
     @commands.command(pass_context = True, is_private = True)
     async def bhavya(self, ctx, message):
         """Sends a message to Bhavya"""
+        u = '<@199436790581559296>'
         await self.bot.say('Kek1')
         b = self.bot.get_channel('199436790581559296')
         await self.bot.say('Kek2')
-        await self.bot.say('{}'.format(b.name))
+        await self.bot.say('{} {}'.format(u, b))
         await self.bot.send_message(b, message)
-        await self.bot.say('Sent message to {}'.format(b.mention))
+        await self.bot.say('Sent message to {}'.format(u))
 
 def setup(bot):
     n = cogtest(bot)
