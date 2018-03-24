d99d83835bac3e86ae971e3ad7732a58cedf17b9
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-24 14:11:50 +0100

    Update cogtest.py

--- cogtest/cogtest.py
+++ cogtest/cogtest.py
@@ -16,20 +16,33 @@ class cogtest:
         bc = discord.utils.get(ctx.message.server.roles, name = 'Bot-Commander')
         u = ctx.message.author 
         if bc in u.roles:
-            m = ('**You have a message from {}:**\n*{}*'.format(ctx.message.author.name, message))
+            m = ('**You have a message from {}:**\n*{}*'.format(u.name, message))
             await self.bot.send_message(user, m)
             await self.bot.say('Sent message to {}'.format(user.name))
         else:
             await self.bot.say('You don\'t have permissions to use this command.')
             
     @commands.command(pass_context = True, no_pm = True)
-    async def pfp(self, ctx, user: discord.Member = None):
+    async def pfp(self, ctx, *, user: discord.Member = None):
         """Sends the user profile picture, if user is none then sends yours"""
         if user is None:
             user = ctx.message.author
             pass
         m = ('Profile Picture for **{}**:\n{}'.format(user.name, user.avatar_url))
         await self.bot.say(m)
+        
+    @commands.command(pass_context = True, no_pm = True)
+    async def game(self, ctx, *, user:discord.Member = None):
+        if user is None:
+            user = ctx.message.author
+            pass
+        if user.game is None:
+            g = '{} is chilling'.format(user.name)
+        elif user.game.url is None:
+            g = '{} is playing {}'.format(user.name, user.game)
+        else:
+            g = '{} is streaming: [{}]({})'.format(user.name, user.game, user.game.url)
+        await self.bot.say(g)
             
 def setup(bot):
     n = cogtest(bot)
