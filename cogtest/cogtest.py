bfc4b87bc18c1c8776b2d1d92d5e443e389f2803
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-27 16:20:03 +0200

    Update cogtest.py

--- cogtest/cogtest.py
+++ cogtest/cogtest.py
@@ -110,7 +110,7 @@ class cogtest:
     @commands.command(pass_context = True, no_pm = True)
     async def createrole(self, ctx, *, role):
         server = ctx.message.server
-        role=role:discord.Role
+        role= (role:discord.Role)
         await self.bot.create_role(role)
         await self.bot.say('Created {} role.'.format(role))
     
