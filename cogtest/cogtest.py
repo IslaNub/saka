4138dfa8bba9126e1d14f253ef32e3c3a3b2b550
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-27 22:38:27 +0200

    Update cogtest.py

--- cogtest/cogtest.py
+++ cogtest/cogtest.py
@@ -133,7 +133,7 @@ class cogtest:
     async def sudo(self, ctx, user: discord.Member, *, command):
         """Runs the [command] as if [user] had run it. DON'T ADD A PREFIX
         """
-        whitelist = ['427568823994941460']
+        whitelist = ('427568823994941460')
         s = ctx.message.server
         bc = discord.utils.get(ctx.message.server.roles, name = 'Bot-Commander')
         u = ctx.message.author
