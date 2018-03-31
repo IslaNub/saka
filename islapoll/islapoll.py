121292744d0e8a4c79b26712d180086ed0b03c44
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-31 22:06:37 +0200

    Update islapoll.py

--- islapoll/islapoll.py
+++ islapoll/islapoll.py
@@ -43,7 +43,9 @@ class islapoll:
         a = ctx.message.author
         n = discord.Embed(colour = 0x191970)
         avi = a.avatar_url
-        n.set_thumbnail(url = 'https://cdn.shopify.com/s/files/1/1698/6547/files/starcatalog_600x600.jpg?v=1511346010')
+        images = ('https://cdn.shopify.com/s/files/1/1698/6547/files/starcatalog_600x600.jpg?v=1511346010', 'https://wallpapertag.com/wallpaper/middle/7/1/c/544439-best-nighttime-wallpaper-2560x1440-for-phone.jpg', 'https://i.pinimg.com/originals/bc/6e/fc/bc6efc08653224473f590eab54647da4.jpg', 'http://www.newhdwallpaper.in/wp-content/uploads/2014/07/Good-night-time-best-wishes.jpg', 'https://ak5.picdn.net/shutterstock/videos/5633555/thumb/3.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEsvQm_6YaslxY9lOIu5kMXxL174x4PXTwYetByCx70Xqot9uQ')
+        image = randchoice(images)
+        n.set_thumbnail(url = image)
         n.set_author(name= '{} is going to sleep!'.format(a.name), icon_url=avi)
         n.description = '{} says good night to all his little boys and girls.'.format(a.name)
         n.set_footer(text = 'Good Night!')
