befee419a6931819c1d74a878048f64de71ffcfe
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-04-04 21:40:02 +0200

    Update islapoll.py

--- islapoll/islapoll.py
+++ islapoll/islapoll.py
@@ -43,9 +43,15 @@ class islapoll:
     async def gn(self, ctx):
         a = ctx.message.author
         IN = '199436790581559296'
-        n = discord.Embed(colour = 0x191970)
+        if a.id = IN:
+            n = discord.Embed(colour = 0x8A0707)
+        else:
+            n = discord.Embed(colour = 0x191970)
         avi = a.avatar_url
-        images = ('https://cdn.shopify.com/s/files/1/1698/6547/files/starcatalog_600x600.jpg?v=1511346010', 'https://wallpapertag.com/wallpaper/middle/7/1/c/544439-best-nighttime-wallpaper-2560x1440-for-phone.jpg', 'https://i.pinimg.com/originals/bc/6e/fc/bc6efc08653224473f590eab54647da4.jpg', 'http://www.newhdwallpaper.in/wp-content/uploads/2014/07/Good-night-time-best-wishes.jpg', 'https://ak5.picdn.net/shutterstock/videos/5633555/thumb/3.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEsvQm_6YaslxY9lOIu5kMXxL174x4PXTwYetByCx70Xqot9uQ')
+        if a.id = IN:
+            images = ('https://goo.gl/images/6GLn4X', 'https://goo.gl/images/3Nfo1P')
+        else:
+            images = ('https://cdn.shopify.com/s/files/1/1698/6547/files/starcatalog_600x600.jpg?v=1511346010', 'https://wallpapertag.com/wallpaper/middle/7/1/c/544439-best-nighttime-wallpaper-2560x1440-for-phone.jpg', 'https://i.pinimg.com/originals/bc/6e/fc/bc6efc08653224473f590eab54647da4.jpg', 'http://www.newhdwallpaper.in/wp-content/uploads/2014/07/Good-night-time-best-wishes.jpg', 'https://ak5.picdn.net/shutterstock/videos/5633555/thumb/3.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEsvQm_6YaslxY9lOIu5kMXxL174x4PXTwYetByCx70Xqot9uQ')
         image = randchoice(images)
         n.set_thumbnail(url = image)
         if a.id == IN:
