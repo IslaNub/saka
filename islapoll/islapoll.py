b74e7fbe15015ff44099e855db4f4c12d37c25c0
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-04-08 13:57:43 +0200

    Update islapoll.py

--- islapoll/islapoll.py
+++ islapoll/islapoll.py
@@ -143,7 +143,7 @@ class islapoll:
             if sm != 'True' and sm != 'False':
                 sm = 'null'
                 pass
-            await self.bot.say('Edited permissions for {} in {}:\n```Send messages= {}\nRead messages= {}```'.format(role.name, channel.name, sm, rm))
+            await self.bot.say('Edited permissions for {} in {}:\n```Send messages= {}\nRead messages= {}```'.format(role, channel, sm, rm))
         else:
             await self.bot.say('You can\'t use this command.')
 
