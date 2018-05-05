826f864494af20f269fa33a3414f8b902e0ac68b
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-05-05 13:45:07 +0200

    Update cogtest.py

--- cogtest/cogtest.py
+++ cogtest/cogtest.py
@@ -324,11 +324,13 @@ class cogtest:
         a = discord.utils.get(server.roles, name = 'Admin')
         if a in u.roles:
             try:
+                x = 0
                 m = await self.bot.say('List of the members with **{}** role:'.format(role.name))
+                count = await self.bot.say('{} members have this role.'.format(x))
                 for member in ctx.message.server.members:
                     if role in member.roles:
                         if len(m.content) > 1800:
-                            m = await self.bot.say(f'{member.name}') 
+                            m = await self.bot.say(f'{member.name}')
                         else:
                             m = await self.bot.edit_message(m, f'{m.content}\n{member.name}')
             except Exception as e:
