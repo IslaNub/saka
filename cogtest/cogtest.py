cece2f7b3b1a05107ee0133a34c1f121cde86959
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-05-06 17:42:14 +0200

    Update cogtest.py

--- cogtest/cogtest.py
+++ cogtest/cogtest.py
@@ -398,7 +398,7 @@ class cogtest:
             z.set_author(name = 'Zrib should be using this...', icon_url = zrib.avatar_url)
             z.description = 'Please use [this](https://discord.gg/royalerecruit) Server for recruiting or searching for a clan.'
             z.title = 'Recruit Server'
-            z.footer(text = 'Have a nice day!', icon_url = ctx.message.author.avatar_url)
+            z.footer(text = 'Have a nice day!')
             #z.set_thumbnail(url = ctx.message.author.avatar_url)
             await self.bot.say(embed = z)
         except Exception as e:
