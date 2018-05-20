a62211f7a72ff363af29a412c67661cbd8d66da8
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-05-20 14:23:23 +0200

    Update cogtest.py

--- cogtest/cogtest.py
+++ cogtest/cogtest.py
@@ -483,7 +483,8 @@ class cogtest:
         col = hex.strip('#')
         hexapi = 'http://www.htmlcsscolor.com/preview/gallery/{}.png'.format(col)
         em = discord.Embed()
-        em.color = discord.Color('0x{}'.format(col))
+        hexcolor = '0x'.format(col)
+        em.color = hexcolor
         em.set_image(url = hexapi)
         await self.bot.say(embed = em)
     
