40c741e5ad5f304990fb49f0ccecfd9e3a6f4d96
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-06-19 13:37:51 +0200

    Update islapoll.py

--- islapoll/islapoll.py
+++ islapoll/islapoll.py
@@ -42,66 +42,31 @@ class islapoll:
     @commands.command(pass_context = True, no_pm = True)
     async def gn(self, ctx):
         a = ctx.message.author
-        IN = '199436790581559296'
-        Alex = '327429593067683840'
-        if a.id == IN:
-            n = discord.Embed(colour = 0x8A0707)
-        else:
-            n = discord.Embed(colour = 0x191970)
+        n = discord.Embed(colour = 0x191970)
         avi = a.avatar_url
         if a.id == IN:
-            images = ('https://i.pinimg.com/originals/67/e1/73/67e173bbadbd41caccf4654760684b19.jpg', 'https://i.pinimg.com/originals/19/9e/e4/199ee4174eab3bfa3142c102e196a2f3.jpg', 'http://2.bp.blogspot.com/-rtUeJkZQp2A/VTeBJYuygDI/AAAAAAAAAnM/RtGsuI1VJ1k/w1200-h630-p-k-no-nu/blood-death-emo-knife-sad-Favim.com-115129.jpg', 'http://2.bp.blogspot.com/-9k9EEkQ1ALQ/T2zFtKavEpI/AAAAAAAACLM/xMJul8_6oTc/s1600/love_hurts_a_lot-764464.jpg')
-        else:
-            images = ('https://cdn.shopify.com/s/files/1/1698/6547/files/starcatalog_600x600.jpg?v=1511346010', 'https://wallpapertag.com/wallpaper/middle/7/1/c/544439-best-nighttime-wallpaper-2560x1440-for-phone.jpg', 'https://i.pinimg.com/originals/bc/6e/fc/bc6efc08653224473f590eab54647da4.jpg', 'http://www.newhdwallpaper.in/wp-content/uploads/2014/07/Good-night-time-best-wishes.jpg', 'https://ak5.picdn.net/shutterstock/videos/5633555/thumb/3.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEsvQm_6YaslxY9lOIu5kMXxL174x4PXTwYetByCx70Xqot9uQ')
+        images = ('https://cdn.shopify.com/s/files/1/1698/6547/files/starcatalog_600x600.jpg?v=1511346010', 'https://wallpapertag.com/wallpaper/middle/7/1/c/544439-best-nighttime-wallpaper-2560x1440-for-phone.jpg', 'https://i.pinimg.com/originals/bc/6e/fc/bc6efc08653224473f590eab54647da4.jpg', 'http://www.newhdwallpaper.in/wp-content/uploads/2014/07/Good-night-time-best-wishes.jpg', 'https://ak5.picdn.net/shutterstock/videos/5633555/thumb/3.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEsvQm_6YaslxY9lOIu5kMXxL174x4PXTwYetByCx70Xqot9uQ')
         image = randchoice(images)
         n.set_thumbnail(url = image)
-        if a.id == IN:
-            n.set_author(name= '{} is going to die!'.format(a.name), icon_url=avi)
-        else:
-            n.set_author(name= '{} is going to sleep!'.format(a.name), icon_url=avi)
-        if a.id == IN:
-            n.description = '{} is dying and says good-bye to all his little boys and girls.'.format(a.name)
-        elif a.id == Alex:
-            n.description = '{} says good night to all his cute boys and girls.'.format(a.name)
-        else:
-            n.description = '{} says good night to all his cute little boys and girls.'.format(a.name)
-        if a.id == IN:
-            n.set_footer(text = 'Enjoy your life!')
-        else:
-            n.set_footer(text = 'Good Night!')
+        n.set_author(name= '{} is going to sleep!'.format(a.name), icon_url=avi)
+        n.description = '{} says good night to all these cute little boys and girls!'.format(a.name)
+        n.set_footer(text = 'Good Night!')
         await self.bot.say(embed = n)
+        await self.bot.delete_message(ctx.message.channel, ctx.message)
         
     @commands.command(pass_context = True, no_pm = True)
     async def gm(self, ctx):
         a = ctx.message.author
-        IN = '199436790581559296'
-        Alex = '327429593067683840'
-        if a.id == IN:
-            n = discord.Embed(colour = 0x5E0000)
-        else:
-            n = discord.Embed(colour = 0x00BFFF)
+        n = discord.Embed(colour = 0x00BFFF)
         avi = a.avatar_url
-        if a.id == IN:
-            images = ('https://i.pinimg.com/originals/59/91/be/5991be64ff7a5bc35cf78b3de1c81ccd.jpg', 'https://i.ytimg.com/vi/epgi78q5OdM/maxresdefault.jpg', 'https://i.ytimg.com/vi/MGHtwbp-wfY/maxresdefault.jpg', 'https://i.pinimg.com/originals/be/b2/22/beb22246dbb74ee3643eb80fe97d111a.jpg', 'http://churchandstate.org.uk/wordpressRM/wp-content/uploads/2015/12/hell.jpg')
-        else:
-            images = ('https://i.ytimg.com/vi/8Hedq2d1H44/maxresdefault.jpg', 'https://cdn.pixabay.com/photo/2016/08/31/17/41/sunrise-1634197_1280.jpg', 'https://techcrunch.com/wp-content/uploads/2015/10/shutterstock_112249904.jpg', 'https://images.unsplash.com/photo-1504270159110-876cfaca9ece?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=874a6efa6345d66b76fde4099fa67757&w=1000&q=80')
+        images = ('https://i.ytimg.com/vi/8Hedq2d1H44/maxresdefault.jpg', 'https://cdn.pixabay.com/photo/2016/08/31/17/41/sunrise-1634197_1280.jpg', 'https://techcrunch.com/wp-content/uploads/2015/10/shutterstock_112249904.jpg', 'https://images.unsplash.com/photo-1504270159110-876cfaca9ece?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=874a6efa6345d66b76fde4099fa67757&w=1000&q=80')
         image = randchoice(images)
         n.set_thumbnail(url = image)
-        if a.id == IN:
-            n.set_author(name= '{} is dead!'.format(a.name), icon_url=avi)
-        else:
-            n.set_author(name= '{} has just woken up!'.format(a.name), icon_url=avi)
-        if a.id == IN:
-            n.description = '{} is peacefully resting in the Darkness...'.format(a.name)
-        elif a.id == Alex:
-            n.description = '{} says good morning to all his cute boys and girls.'.format(a.name)
-        else:
-            n.description = '{} says good morning to all his cute little boys and girls.'.format(a.name)
-        if a.id == IN:
-            n.set_footer(text = 'Warm regards from the Hell!')
-        else:
-            n.set_footer(text = 'Good Morning!')
+        n.set_author(name= '{} has just woken up!'.format(a.name), icon_url=avi)
+        n.description = '{} says good morning to all these cute little boys and girls!'.format(a.name)
+        n.set_footer(text = 'Good Morning!')
         await self.bot.say(embed = n)
+        await self.bot.delete_message(ctx.message.channel, ctx.message)
 
     #@commands.group(pass_context = True, no_pm = True, invoke_without_command = True)
     #async def editchannelperms(self, ctx, role : discord.Role, channel : discord.Channel, read_messages, send_messages):
