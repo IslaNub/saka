26abde863c5fc360d07ddbc0c6179a2f8063d128
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-06-19 17:07:12 +0200

    Update cogtest.py

--- cogtest/cogtest.py
+++ cogtest/cogtest.py
@@ -499,7 +499,7 @@ class cogtest:
             async with session.get(url) as resp:
                 data = await resp.json()
         em.set_image(url = hexapi)
-        #em.color = int('0x' + hex, 16)
+        em.color = int('0x' + hex, 16)
         #em.add_field(name = 'Name:', value = data["colors"][0]["name"], inline = True)
         #em.add_field(name = 'Hex:', value = data["colors"][0]["hex"]).upper(), inline = True)
         #em.add_field(name = 'RBG:', value = 'R: ' + str(data["colors"][0]["rgb"]["r"]) + '; G: ' + str(data["colors"][0]["rgb"]["g"]) + '; B: ' + str(data["colors"][0]["rgb"]["b"]), inline = True)
