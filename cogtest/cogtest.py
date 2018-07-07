c86a024cb283eb7b8e6cd66f9381f1a07b02200b
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-07-08 01:41:23 +0200

    Update cogtest.py

--- cogtest/cogtest.py
+++ cogtest/cogtest.py
@@ -601,7 +601,7 @@ class cogtest:
             user = ctx.message.author
         if user.id == '199436790581559296':
             n = random.randint(90, 100)
-        elif user.id == '200467543968710656':
+        elif user.id == '200467543968710656' or user.name == 'Contrast' and user.discriminator == '5761':
             n = random.randint(0, 10)
         else:
             n = random.randint(0, 100)
