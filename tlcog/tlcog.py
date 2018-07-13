90346652f7ee9485197c4fb75efc516ff3d0d726
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-07-13 19:54:19 +0200

    Update tlcog.py

--- tlcog/tlcog.py
+++ tlcog/tlcog.py
@@ -51,19 +51,7 @@ class tlcog:
     async def owner(self, ctx):
         await self.bot.say('{} is the Owner of this Bot.'.format(self.isla().name))
     
-    @commands.command(pass_context=True)
-    async def pingtime(self, ctx):
-        """Pong."""
-        t1 = time.perf_counter()
-        await self.bot.send_typing(ctx.message.channel)
-        t2 = time.perf_counter()
-        thedata = ("**Pong.**\nTime: " + str(round((t2-t1)*1000)) + "ms")
-        color = ''.join([choice('0123456789ABCDEF') for x in range(6)])
-        color = int(color, 16)
-        data = discord.Embed(description=thedata, colour=discord.Colour(value=color))
-        await self.bot.say(embed=data)
 
-    
     #MATHS
     def comb(self, x, y, rounding):
         if (x).is_integer():
