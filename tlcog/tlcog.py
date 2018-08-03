c3dbd00f03e81194dc99dff26bd760164bc14c15
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-08-03 16:18:49 +0100

    Update tlcog.py

--- tlcog/tlcog.py
+++ tlcog/tlcog.py
@@ -82,7 +82,7 @@ class tlcog:
     async def aetest(self, ctx, a, b, c):
         try:
             await self.bot.say(f"Random test because AE is cool, {self.ae(a = a, b = b, c = f'{c}'}")
-         except Exception as e:
+        except Exception as e:
             await self.bot.say(e)
         
     #MATHS
