2775628882597332699b6727aac6feb3a9814297
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-09-03 02:19:04 +0200

    Update tlcog.py

--- tlcog/tlcog.py
+++ tlcog/tlcog.py
@@ -214,14 +214,14 @@ class tlcog:
         if animal.lower().strip() in animals_list:
             await self.bot.say(embed = self.animals(animal = animal))
         elif animal.lower().strip() == 'list':
-            m = 'Here the list of the current available animals:\n```css\n' + '\n'.join(sorted(animals_list, reverse = False)) \
-                 + '```\nIf you have any requirement please use the `+contact` command.'
+            m = 'Here the list of the current available animals:\n```css\n' + '\n'.join(sorted(animals_list, reverse = False)) + \
+                 '```\nIf you have any requirement please use the `+contact` command.'
             await self.bot.send_message(ctx.message.author, m)
             m2 = await self.bot.say('Sent a message in your DMs {}.'.format(ctx.message.author.mention))
             await asyncio.sleep(5)
             await self.bot.delete_message(m2)
         else:
-            await self.bot.say('Not a whilelisted animal, for more info use `+animal list` command.'
+            await self.bot.say('Not a whilelisted animal, for more info use `+animal list` command.')
         
         
 
