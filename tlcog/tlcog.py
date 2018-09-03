41a15f991402153bd911e00f9f59b75cabc8bb2e
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-09-03 02:31:06 +0200

    Update tlcog.py

--- tlcog/tlcog.py
+++ tlcog/tlcog.py
@@ -223,7 +223,7 @@ class tlcog:
             msg = await self.bot.send_message(ctx.message.author, m)
             msg2 = await self.bot.say('Sent a message in your DMs {}.'.format(ctx.message.author.mention))
             await asyncio.sleep(5)
-            await self.bot.delete_messages([msg1, msg2])
+            await self.bot.delete_messages([msg, msg2])
         else:
             await self.bot.say('Not a whilelisted animal, for more info use `+animal list` command.')
         
