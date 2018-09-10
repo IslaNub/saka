2a97f07826ba4a44ba485d396e2d1a94ad306576
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-09-10 21:15:28 +0200

    Update tlcog.py

--- tlcog/tlcog.py
+++ tlcog/tlcog.py
@@ -227,7 +227,7 @@ class tlcog:
         else:
             await self.bot.say('Not a whilelisted animal, for more info use `+animal list` command.')
         
-    @commands.command(pass_context = True
+    @commands.command(pass_context = True)
     async def beta(self, ctx):
         u = ctx.message.author
         if ctx.message.channel.is_private is True:
