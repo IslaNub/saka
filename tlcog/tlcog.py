c6368472ba798b10b7ba613c57aab50116d1aa35
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-10-04 16:40:23 +0200

    Update tlcog.py

--- tlcog/tlcog.py
+++ tlcog/tlcog.py
@@ -329,7 +329,7 @@ class tlcog:
                 good_moods = ['good', 'fine', 'great', 'amazing', 'well']
                 if any(mood in m0.content.lower() for mood in good_moods):
                     mood0 = message.content.lower().split()
-                    mood1 = list([mood for mood in mood0 and if mood in good_moods])
+                    mood1 = list([mood for mood in mood0 if mood in good_moods])
                     await self.bot.send_message(c, 'That\'s awesome! I\'m doing {} as well!'.format(mood1))
                     return
             except:
