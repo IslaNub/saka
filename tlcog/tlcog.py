9794187582663caf7123e6b0d66e86062a09dc23
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-10-09 18:02:47 +0200

    Update tlcog.py

--- tlcog/tlcog.py
+++ tlcog/tlcog.py
@@ -316,8 +316,9 @@ class tlcog:
         if reaction.message.channel.id == '453454838974513152' and reaction.emoji == e:
             try:
                 r = discord.utils.get(reaction.message.server.roles, name = 'LCL')
-                r = discord.utils.get(reaction.message.server.roles, name = 'Clash Royale')
+                r1 = discord.utils.get(reaction.message.server.roles, name = 'Clash Royale')
                 await self.bot.add_roles(user, r)
+                await self.bot.remove_roles(user, r1)
                 await self.bot.send_message(reaction.message.channel, 'Done.')
                 return
             except Exception as ex:
@@ -334,8 +335,9 @@ class tlcog:
         if reaction.message.channel.id == '453454838974513152' and reaction.emoji == e:
             try:
                 r = discord.utils.get(reaction.message.server.roles, name = 'LCL')
-                r = discord.utils.get(reaction.message.server.roles, name = 'Clash Royale')
+                r1 = discord.utils.get(reaction.message.server.roles, name = 'Clash Royale')
                 await self.bot.remove_roles(user, r)
+                await self.bot.remove_roles(user, r1)
                 await self.bot.send_message(reaction.message.channel, 'Done.')
                 return
             except Exception as ex:
