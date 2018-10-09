3105fed9fb9f5cec2d089d3755778b58e40f11c7
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-10-09 17:48:36 +0200

    Update tlcog.py

--- tlcog/tlcog.py
+++ tlcog/tlcog.py
@@ -312,26 +312,39 @@ class tlcog:
         await self.bot.process_commands(new_msg)
 
     async def on_reaction_add(self, reaction, user:discord.Member):
-        e = discord.utils.get(reaction.message.server.emojis, name = 'LCL_logo')
+        e = discord.utils.get(reaction.message.server.emojis, name = 'LCL_Logo')
         if reaction.message.channel.id == '453454838974513152' and reaction.emoji == e:
             try:
                 r = discord.utils.get(reaction.message.server.roles, name = 'LCL')
-                r = discord.utils.get(reaction.message.server.roles, name = 'Clash Royale')
                 await self.bot.add_roles(user, r)
                 await self.bot.send_message(reaction.message.channel, 'Done.')
-            except:
-                pass
+                return
+            except Exception as ex:
+                u = reaction.message.server.get_member_named('IslaWoof')
+                await self.bot.send_message(reaction.message.channel, ex)
+                return
+        else:
+            u = reaction.message.server.get_member_named('IslaWoof')
+            await self.bot.send_message(reaction.message.channel, ex)
+            return
         
     async def on_reaction_remove(self, reaction, user:discord.Member):
-        e = discord.utils.get(reaction.message.server.emojis, name = 'LCL_logo')
+        e = discord.utils.get(reaction.message.server.emojis, name = 'LCL_Logo')
         if reaction.message.channel.id == '453454838974513152' and reaction.emoji == e:
             try:
                 r = discord.utils.get(reaction.message.server.roles, name = 'LCL')
-                r = discord.utils.get(reaction.message.server.roles, name = 'Clash Royale')
                 await self.bot.remove_roles(user, r)
                 await self.bot.send_message(reaction.message.channel, 'Done.')
-            except:
-                pass
+                return
+            except Exception as ex:
+                u = reaction.message.server.get_member_named('IslaWoof')
+                await self.bot.send_message(reaction.message.channel, ex)
+                return
+        else:
+            u = reaction.message.server.get_member_named('IslaWoof')
+            await self.bot.send_message(reaction.message.channel, ex)
+            return
+
         
         
     #CLEVERBOT, WIP
