3b7463df56701caac84c9049e6e91a772576018d
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-10-20 18:04:22 +0200

    Update tlcog.py

--- tlcog/tlcog.py
+++ tlcog/tlcog.py
@@ -397,6 +397,8 @@ class tlcog:
         
         
         Example: "+cutify Now I really love Discord!" will return "Nyow I weally love Discowd!", while "+cutify 1234567890 0987654321" will cutify the message in the channel with the ID "0987654321" and with message ID "1234567890" """
+        if '@everyone' in message_or_messageID or '@everyone' in channel_id or '@here' in message_or_messageID or '@here' in channel_id:
+            await self.bot.say('Dont\'t attempt to ping `@everyone` and/or `@here` via the Bot, thanks!'
         if channel_id is None and message_or_messageID is None:
             await self.bot.say('Please provide a valid channel & message ID.')
             return
@@ -414,7 +416,7 @@ class tlcog:
             except:
                 await self.bot.say('Invalid message, please provide a valid message ID.')
                 return
-            m = m.content
+            m = m.clean_content
             await self.bot.say(self.cutify_worker(m = m))
         except:
             if channel_id is None:
