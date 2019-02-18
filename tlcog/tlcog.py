a43c476b934f4183207dbe2d2035a64b0f01343e
IslaWoof <34550190+IslaWoof@users.noreply.github.com>
2019-02-18 10:33:48 +0100

    Update tlcog.py

--- tlcog/tlcog.py
+++ tlcog/tlcog.py
@@ -569,24 +569,6 @@ class tlcog:
                 msg.set_footer(text = 'Thanks for helping us testing our new Shop!')
                 await self.bot.edit_message(message, plain_msg, embed = msg)
                 break
-    
-    
-    #ALICIZATION
-    
-    
-    async def on_message(self, message):
-        c = message.channel
-        u = message.author
-        s = message.server
-        msg = message.content.lower().strip()
-        await self.bot.say(msg)
-        if msg == 'system call.':
-            await self.bot.say(msg)
-            sacred_art_start = await self.bot.wait_for_message(check = lambda x: x.author == msg.author and x.channel == msg.channel, timeout = 30)
-            await self.bot.say(sacred_art_start)
-            command_evok = ['generate', 'element']
-            element = re.sub('|'.join(command_evok), '', sacred_art_start.lower()).strip()
-            await self.bot.say(element)
             
 
 def setup(bot):
