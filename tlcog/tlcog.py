bdb469ddc6c13e68cafc2b3b041cce523dec28c0
IslaWoof <34550190+IslaWoof@users.noreply.github.com>
2019-02-18 10:20:28 +0100

    Update tlcog.py

--- tlcog/tlcog.py
+++ tlcog/tlcog.py
@@ -579,14 +579,13 @@ class tlcog:
         u = message.author
         s = message.server
         msg = message.content
-        if msg.lower().strip() = 'system call':
+        if msg.lower().strip() == 'system call':
             sacred_art_start = await self.bot.wait_for_message(check = lambda x: x.author == ctx.message.author and x.channel == ctx.message.channel, timeout = 30)
             command_evok = ['generate', 'element']
             element = re.sub('|'.join(command_evok), '', sacred_art_start.lower()).strip()
             await self.bot.say(element)
             
-            
-    
+
 def setup(bot):
     n = tlcog(bot)
     bot.add_cog(n)
