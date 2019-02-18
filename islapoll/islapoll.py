450b0c568b51f3c2a943f0edf155df46a2d2ffa8
IslaWoof <34550190+IslaWoof@users.noreply.github.com>
2019-02-18 14:05:40 +0100

    Update islapoll.py

--- islapoll/islapoll.py
+++ islapoll/islapoll.py
@@ -170,8 +170,8 @@ class islapoll:
         msg = message.content.lower().strip()
         if msg == 'system call.':
             sacred_art_start = await self.bot.wait_for_message(check = lambda x: x.author == msg.author and x.channel == msg.channel, timeout = 30)
-            command_evok = ['generate', 'element', '.']
-            element = re.sub('|'.join(command_evok), '', sacred_art_start.lower()).strip()
+            command_evok = ['generate', 'element.']
+            element = re.sub('|'.join(command_evok), '', sacred_art_start.content.lower()).strip()
             await self.bot.send_message(c, element)
             
 def setup(bot):
